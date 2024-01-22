8254a0c7c579c122e19cc9b6aefb28c683088ef6
myNameHtml <kyawhtethtun65@gmail.com>
2024-01-22 10:29:57 +0100

    first commit

---
+++ src/main/java/at/htlleonding/Kinalyze/KinalyzeApplication.java
@@ -0,0 +1,13 @@
+package at.htlleonding.Kinalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinalyzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/htlleonding/Kinalyze/KinalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlleonding.Kinalyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinalyzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
